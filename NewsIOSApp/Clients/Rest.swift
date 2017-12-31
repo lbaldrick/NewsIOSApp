70094a91390bd49124d8f63cfd39d6470154cebe
Michael Lee Baldrick <michaelbaldrick@Michaels-iMac.local>
2017-12-31 00:14:43 +0000

    Create rest client

---
+++ NewsIOSApp/Clients/Rest.swift
@@ -0,0 +1,51 @@
+//
+//  Rest.swift
+//  NewsIOSApp
+//
+//  Created by Michael Lee Baldrick on 29/12/2017.
+//  Copyright © 2017 Michael Lee Baldrick. All rights reserved.
+//
+
+import Foundation
+
+
+class Rest {
+    
+    private static let baseUrl: String = ""
+    
+    private static func createRequest(urlString: String, method: String) -> URLRequest {
+        let url = URL(string: urlString)!
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = method
+        return request
+    }
+    
+    private static func makeRequest(request: URLRequest, completion: @escaping (Any?) -> Void) ->  URLSessionDataTask {
+        let task = URLSession.shared.dataTask(with: request, completionHandler: { (data, response, error) in
+            guard let data = data else {
+                completion(nil)
+                return
+            }
+            guard let json = try? JSONSerialization.jsonObject(with: data, options: .mutableContainers) else {
+                completion(nil)
+                return
+            }
+            
+            completion(json)
+        })
+        
+        return task;
+    }
+    
+    static func get(url: String, completion: @escaping (Any?) -> Void) {
+        let request = createRequest(urlString: url, method: "GET")
+        let task = makeRequest(request: request, completion: completion)
+        
+        task.resume();
+    }
+    
+    static func post(url: String, body: String) {
+        
+    }
+}
--- NewsIOSApp/ViewController.swift
+++ NewsIOSApp/ViewController.swift
@@ -19,6 +19,12 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func showMessage(sender: UIButton) {
+        Rest.get(url: "http://192.168.0.39:3003/news/all") { (any) in
+            print(any!)
+        }
+    }
 
 
 }
